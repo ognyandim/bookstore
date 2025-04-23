93a034885343692199844516db1270b5ed1b5389
Ognyan Dimitrov <ognyandimitrov@kpmg.com>
2025-04-23 15:34:02 +0300

    Introduce Book CrudAppService with DTOs and Service interfaces and seed

--- Acme.BookStore/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ Acme.BookStore/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,3 +1,5 @@
+using Acme.BookStore.Books;
+
 using AutoMapper;
 
 namespace Acme.BookStore;
@@ -9,5 +11,9 @@ public class BookStoreApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
+
     }
 }
---
+++ Acme.BookStore/src/Acme.BookStore.Domain/Data/BookStoreDataSeederContributor.cs
@@ -0,0 +1,63 @@
+﻿using Acme.BookStore.Books;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Guids;
+
+using Volo.Abp.MultiTenancy;
+
+namespace Acme.BookStore.Data
+{
+    internal class BookStoreDataSeederContributor : IDataSeedContributor, ITransientDependency
+    {
+        private readonly IRepository<Book, Guid> _bookRepository;
+        private readonly IGuidGenerator _guidGenerator;
+        private readonly ICurrentTenant _currentTenant;
+
+        public BookStoreDataSeederContributor(
+            IRepository<Book, Guid> bookRepository,
+            IGuidGenerator guidGenerator,
+            ICurrentTenant currentTenant)
+        {
+            _bookRepository = bookRepository;
+            _guidGenerator = guidGenerator;
+            _currentTenant = currentTenant;
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            //using (_currentTenant.Change(context?.TenantId))
+            //{
+                if (await _bookRepository.GetCountAsync() <= 0)
+            {
+                await _bookRepository.InsertAsync(
+                    new Book
+                    {
+                        Name = "1984",
+                        Type = BookType.Dystopia,
+                        PublishDate = new DateTime(1949, 6, 8),
+                        Price = 19.84f
+                    },
+                    autoSave: true
+                );
+
+                await _bookRepository.InsertAsync(
+                    new Book
+                    {
+                        Name = "The Hitchhiker's Guide to the Galaxy",
+                        Type = BookType.ScienceFiction,
+                        PublishDate = new DateTime(1995, 9, 27),
+                        Price = 42.0f
+                    },
+                    autoSave: true
+                );
+            }
+        }
+    }
+}
