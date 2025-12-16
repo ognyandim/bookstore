32a6ee6777e93aa72bf676dfb056085102e8c2ab
Ognyan Dimitrov <ognyan.dimitrov@kpmg.com>
2025-12-16 18:01:52 +0200

    fix(books): Make props required

--- Acme.BookStore/src/Acme.BookStore.Application.Contracts/Books/AuthorDto.cs
+++ Acme.BookStore/src/Acme.BookStore.Application.Contracts/Books/AuthorDto.cs
@@ -5,8 +5,8 @@ namespace Acme.BookStore.Books
     public class AuthorDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public DateTime BirthDate { get; set; }
-        public string ShortBio { get; set; }
+        public required string ShortBio { get; set; }
     }
 }
